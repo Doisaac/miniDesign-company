fc83dca0c64726efc9b8628c5e437d48454d9de7
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-04-30 21:58:30 -0600

    🍔add responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+const hamburger = document.querySelector('.navbar__header .navbar .navbar__list .navbar__hamburger');
+const mobile_menu = document.querySelector('.navbar__header .navbar .navbar__list ul');
+const header = document.querySelector('.navbar__header .navbar__container');
+const links = document.querySelectorAll('.navbar-link')
+const headerBackground = document.querySelector('.navbar__header');
 
+// WHEN THE HAMBURGER IS CLICKED -> MENU APPEARS
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+// WHEN A LINK OF THE NAVBAR IS CLICKED, THE MOBILE MENU CLOSE
+links.forEach(function(link) {
+    link.addEventListener('click', () => {
+        mobile_menu.classList.remove('active');
+        hamburger.classList.remove('active')
+    })
+});
