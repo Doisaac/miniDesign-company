d1a14a6b3f17dd492860a8ba6455e5f541d1f3b8
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-05-01 16:43:28 -0600

    🎚️ add slider and navbar effect

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-const hamburger = document.querySelector('.navbar__header .navbar .navbar__list .navbar__hamburger');
-const mobile_menu = document.querySelector('.navbar__header .navbar .navbar__list ul');
+const hamburger = document.querySelector('.navbar__header .nav .navbar__list .navbar__hamburger');
+const mobile_menu = document.querySelector('.navbar__header .nav .navbar__list ul');
 const header = document.querySelector('.navbar__header .navbar__container');
 const links = document.querySelectorAll('.navbar-link')
 const headerBackground = document.querySelector('.navbar__header');
+const carouselControls = document.querySelector('.carousel-indicators')
 
 // WHEN THE HAMBURGER IS CLICKED -> MENU APPEARS
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
+    carouselControls.classList.toggle('deactive')
 });
 
 // WHEN A LINK OF THE NAVBAR IS CLICKED, THE MOBILE MENU CLOSE
