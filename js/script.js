15a19bafd6796d22eb2c1e65ce2cf36c0ecc560d
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-05-25 15:39:40 -0600

    📄add FAQs to index

--- js/script.js
+++ js/script.js
@@ -53,3 +53,25 @@ columns.forEach(click => {
     });
   });
 });
+
+/* ACCORDION */
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion, index) => {
+  const header = accordion.querySelector(".accordion__header");
+  const content = accordion.querySelector(".accordion__content");
+
+  header.addEventListener("click", () => {
+    const isOpen = content.style.height === `${content.scrollHeight}px`;
+
+    accordions.forEach((a, i) => {
+      const c = a.querySelector(".accordion__content");
+      const ic = a.querySelector(".accordion__icon");
+
+      c.style.height = i === index && !isOpen ? `${c.scrollHeight}px` : "0px";
+      ic.classList.toggle("rotate-effect", i !== index || !isOpen);
+      ic.classList.toggle("rotate-effect", i === index && !isOpen);
+    });
+  });
+});
+
